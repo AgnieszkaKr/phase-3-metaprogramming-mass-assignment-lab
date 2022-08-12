ff0fecaf9c07febabaf46eacc6e9af6318125d76
Agnes <agsawicka@gmail.com>
2022-08-12 18:13:46 -0400

    commit tree 81c9ffe56b18407062142c8b7bbf99f7ea001d10

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
-  # your code here
+    def initialize(atributes)
+      atributes.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+
+      end 
+    end
+
+
 end
+
+
+# setter
+# def name=(name)
+#   @name=name
+# end 
+
+# getter
+# def name
+#   @name
+# end
